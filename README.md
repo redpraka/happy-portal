a70a9eaf083765f1c224919dd0fb2c32ae528972
redpraka <u.reddiprakash@gmail.com>
2020-11-08 16:29:56 +0800

    Initial Version

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
+# StocksData
+
+## API to hit
+
+*JSON API URL :*
+`https://jsonmock.hackerrank.com/api/stocks?date=<date>`
+
+## Expected Outputs when result found
+
+- `Open: {open}`
+- `Close: {close}`
+- `High: {high}`
+- `Low: {low}`
+
+## Expected Output when NO results found
+
+In case of no results render below div
+`<div data-testid="no-result">No Results Found</div>`
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
+import StockData from './components/StockData'
 import './App.css';
+const title = "Stock Data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>
+    <nav className="app-header layout-row align-items-center justify-content-center">
+      <div className="layout-row align-items-center">
+        <img alt="" src={logo} className="logo"/>
+        <h4 id="app-title" data-testid="app-title" className="app-title">{title}</h4>
+      </div>
+    </nav>
+      <StockData/>    
+  </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
